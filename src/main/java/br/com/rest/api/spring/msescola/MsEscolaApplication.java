f93121ef6e9c98c37e7c097054fbea7b05892643
Gabriel Souza <gabriel.souza@aisdigital.com.br>
2020-04-19 13:14:02 -0300

    Criação do projeto

---
+++ src/main/java/br/com/rest/api/spring/msescola/MsEscolaApplication.java
@@ -0,0 +1,13 @@
+package br.com.rest.api.spring.msescola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEscolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEscolaApplication.class, args);
+	}
+
+}
